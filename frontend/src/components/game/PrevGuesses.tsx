b8d5533fb595bf8563e9f600b2f013127fc55989
maple <maple.devstack@gmail.com>
2026-08-01 01:41:31 +0530

    feat: enhance guess display with correct answer highlighting

--- frontend/src/components/game/PrevGuesses.tsx
+++ frontend/src/components/game/PrevGuesses.tsx
@@ -9,16 +9,33 @@ const PrevGuesses = () => {
     <div className="w-full">
       <table className="-mt-6 -mb-6 w-full border-separate border-spacing-y-2 p-4">
         <tbody>
-          {session.guesses.map((guess) => (
-            <tr key={`${guess.type}-${guess.name}`} className="bg-red-500/10">
-              <td className="rounded-l-md border-y border-l border-red-500/20 p-2">
-                {guess.name}
-              </td>
-              <td className="border-y border-red-500/20 p-2 capitalize italic">
-                {guess.type}
-              </td>
-            </tr>
-          ))}
+          {session.guesses.map((guess, index) => {
+            const isCorrect =
+              session.status === "won" && index === session.guesses.length - 1
+
+            return (
+              <tr
+                key={`${guess.type}-${guess.name}`}
+                className={isCorrect ? "bg-green-500/10" : "bg-red-500/10"}
+              >
+                <td
+                  className={`rounded-l-md border-y border-l p-2 ${
+                    isCorrect ? "border-green-500/20" : "border-red-500/20"
+                  }`}
+                >
+                  {guess.name}
+                </td>
+
+                <td
+                  className={`border-y p-2 capitalize italic ${
+                    isCorrect ? "border-green-500/20" : "border-red-500/20"
+                  }`}
+                >
+                  {guess.type}
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
